bb4a116fa55c5e813119fbce53ec72367b9f0cdf
JUNIOR LINTHON VARGAS SOTOMAYOR <JUVARGAS@MINEDU.GOB.PE>
2023-12-28 11:00:31 -0500

    anima

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import { Hono } from "https://deno.land/x/hono@v3.11.9/mod.ts"
-import { serveStatic } from "https://deno.land/x/hono@v3.11.9/middleware.ts"
+import { cors, serveStatic } from "https://deno.land/x/hono@v3.11.9/middleware.ts"
 import { streamSSE } from "https://deno.land/x/hono@v3.11.9/helper/streaming/index.ts"
 
 const db = await Deno.openKv();
@@ -11,6 +11,8 @@ interface LastVisit {
     city: string,
     flag: string
 }
+
+app.use(cors())
 
 app.get('/', serveStatic({path: './index.html'}))
 
